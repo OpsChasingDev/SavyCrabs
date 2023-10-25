acbdb93b697bb6cc02eea46be970c034423a5c0f
tekenator <tekenator@gmail.com>
2023-10-25 15:09:03 -0400

    custom object updated

--- Server Report.ps1
+++ Server Report.ps1
@@ -1,9 +1,14 @@
 # get computer name and os version
-$Computerinfo = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
+$Computerinfo = Get-ComputerInfo
 
 # get number of disks
 $Diskinfo = Get-Disk
 
 # custom object for disk count
-$obj = [PSCustomObject]@{LogicalDiskCount = $Diskinfo.Count}
+$obj = [PSCustomObject]@{
+    LogicalDiskCount = $Diskinfo.Count
+    ComputerName = $Computerinfo.CsDNSHostName
+    OsVersion = $Computerinfo.OsVersion
+}
+
 Write-Output $obj
