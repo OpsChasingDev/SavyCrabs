95d021b30059de835998dfb0a4d41871f1a029c6
tekenator <tekenator@gmail.com>
2023-10-25 14:51:03 -0400

    Added command to show number of drives

--- Server Report.ps1
+++ Server Report.ps1
@@ -1,3 +1,7 @@
 # get computer name and OS version
-$info = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
-$info
+$Computerinfo = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
+$Computerinfo
+
+# get number of disks
+$Diskinfo = Get-Disk
+$Diskinfo.count
