85da4dcedc7f5c77ae3f66e57016ab895ecdae48
tekenator <tekenator@gmail.com>
2023-11-01 15:27:15 -0400

    added total memory to object

--- Server Report.ps1
+++ Server Report.ps1
@@ -10,13 +10,22 @@ $FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
 # get number of cores
 $CPUCore = Get-CimInstance CIM_Processor
 
+# get total memory capacity
+$MemoryCapacity = Get-CimInstance CIM_PhysicalMemory
+$MemorySum = 0
+foreach ($m in $MemoryCapacity) {
+    # take capacity property value and add together
+    $memorysum = $memorysum + $m.capacity
+}
+
 # custom object for disk count
 $obj = [PSCustomObject]@{
     LogicalDiskCount = $Diskinfo.Count
     ComputerName = $Computerinfo.CsDNSHostName
     OsVersion = $Computerinfo.OsVersion
-    FreeSpace_GB  = [MATH]::Round(($FreeSpace.Free /1gb),2)
+    FreeSpace_GB  = [MATH]::Round(($FreeSpace.Free / 1gb),2)
     CPUCore = $CPUCore.NumberofCores
+    TotalMemory = [Math]::Round(($MemorySum / 1gb), 0)
 }
 
 Write-Output $obj
