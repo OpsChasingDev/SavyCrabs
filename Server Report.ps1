7ca9b7487ec4200f5631cbd2dab813b30afae652
tekenator <tekenator@gmail.com>
2023-11-01 14:22:12 -0400

    edited freespace property

--- Server Report.ps1
+++ Server Report.ps1
@@ -4,11 +4,15 @@ $Computerinfo = Get-ComputerInfo
 # get number of disks
 $Diskinfo = Get-Disk
 
+# get free system space
+$FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
+
 # custom object for disk count
 $obj = [PSCustomObject]@{
     LogicalDiskCount = $Diskinfo.Count
     ComputerName = $Computerinfo.CsDNSHostName
     OsVersion = $Computerinfo.OsVersion
+    FreeSpace = $FreeSpace.Free
 }
 
 Write-Output $obj
